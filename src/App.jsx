9719e6b52acfe2b81ab8fb983930677fa425c765
samuel <samuel.vcosta@al.infnet.edu.br>
2026-02-14 17:12:34 -0300

    resolvendo merge

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import './styles.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePages from './pages/HomePages/HomePages.jsx';
 import DuvidasPage from './pages/DuvidasPages/Duvidas.jsx';
-<<<<<<< HEAD
 import LoginPages from './pages/LoginPages/LoginPages.jsx';
 import Cadastro from './pages/Cadastro/Cadastro.jsx';
 import { ToastContainer } from 'react-toastify';
-=======
 import AgendamentosPages from './pages/AgendamentosPages/Agendamentos.jsx';
->>>>>>> d13b2c8aa63d687786704f8223e5e7d1f230717e
+
 
 
 const router = createBrowserRouter([
@@ -25,17 +23,16 @@ const router = createBrowserRouter([
     path: "Duvidas",
     element: <DuvidasPage />,
   },
-<<<<<<< HEAD
+
   {
     path: "Cadastro",
     element: <Cadastro />,
-  }
-=======
+  },
    {
     path: "agendamentos",
     element: <AgendamentosPages />,
   },
->>>>>>> d13b2c8aa63d687786704f8223e5e7d1f230717e
+
 
 ]);
 export default function Routes() {
