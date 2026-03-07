9494f19cb5bf42b57e1bfa79d39cbf52559b2801
samuel <samuel.vcosta@al.infnet.edu.br>
2026-03-07 10:32:55 -0300

    consumo da api

--- src/pages/HomePages/HomePages.jsx
+++ src/pages/HomePages/HomePages.jsx
@@ -4,10 +4,17 @@ import Header from '../../components/Header/Header';
 import Topo from '../../components/TopoComponent/Topo';
 import Pesquisa from '../../components/Pesquisa/Pesquisa';
 import InfiniteCarousel from '../../components/CarrocelMain/CarrocelMain';
+import { useState, useEffect } from 'react';
 
 
 export default function HomePage() {
+    const [listaDicas, setListaDicas] = useState(null);
     
+    useEffect(() => {
+        fetch("/api/dicas-saude/dicas-saude.json")
+        .then((response) => response.json())
+        .then((result) => setListaDicas(result))
+    }, []);
 
     return (
         <div className={Styles.HomePage}>
@@ -15,6 +22,15 @@ export default function HomePage() {
             <Topo />
             <Pesquisa />
             <InfiniteCarousel />
+            <div>
+                <p>Dicas de Saúde</p>
+                <div className={Styles.dicas}>
+                    <div className={Styles.dica}>
+                        <p>{listaDicas?.[1]?.title}</p>
+                        <img src={listaDicas?.[1]?.image} alt={listaDicas?.[0]?.title} />
+                    </div>
+                </div>
+            </div>
         </div>   
     )
 }
